d2ebe2c58d2cc12db2ec8330aac7124822ce65d8
anothersalad <48966874+another-salad@users.noreply.github.com>
2025-01-02 13:53:56 +0000

    more hacks yes

--- src/app.py
+++ src/app.py
@@ -5,15 +5,28 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/api")
-def sensor_data():
+def _get_temp_data():
     i2c = board.I2C()
     sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
-    return jsonify({
-        "temperature_celsius": sensor.temperature,
-        "humidity": sensor.humidity,
+    return {
+        "temperature_celsius": round(sensor.temperature, 2),
+        "humidity": round(sensor.humidity, 2),
         "gas": sensor.gas,
-    })
+    }
+
+@app.route("/api")
+def sensor_data():
+    return jsonify(_get_temp_data())
+
+
+@app.route("/")
+def home():
+    data = _get_temp_data()
+    return f"""
+    <h1>Temperature Sensor</h1>
+    <p>Temperature: {data['temperature_celsius']}</p>
+    <p>Humidity: {data['humidity']}</p>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
