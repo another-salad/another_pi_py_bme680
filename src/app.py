df37fde77d5d1f1fc48b3a92e44978cb9ca5d7ae
anothersalad <48966874+another-salad@users.noreply.github.com>
2025-01-03 00:56:09 +0000

    better api routes, rename hackery

--- src/app.py
+++ src/app.py
@@ -1,9 +1,21 @@
 import board
 import adafruit_bme680
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect
+
+from pathlib import Path
 
 app = Flask(__name__)
+
+sensor_name_file = Path("./sensor_name.txt")
+
+
+def _get_sensor_name():
+    return sensor_name_file.read_text().strip()
+
+
+def _set_sensor_name(name):
+    return sensor_name_file.write_text(name, encoding="utf-8")
 
 
 def _get_temp_data():
@@ -13,21 +25,49 @@ def _get_temp_data():
         "temperature_celsius": round(sensor.temperature, 2),
         "humidity": round(sensor.humidity, 2),
         "gas": sensor.gas,
+        "sensor_name": _get_sensor_name(),
     }
 
 
-@app.route("/api")
+@app.route("/api/data")
 def sensor_data():
     return jsonify(_get_temp_data())
 
 
-@app.route("/")
-def home():
+@app.route("/api/rename", methods=["POST"])
+def set_sensor_name():
+    _set_sensor_name(request.form.get("sensor_name"))
+    return redirect("/")
+
+
+@app.route("/rename")
+def rename():
+    return """
+    <h1>Rename Sensor</h1>
+    <form action="/api/rename" method="post">
+        <label for="sensor_name">Sensor Name:</label><br>
+        <input type="text" id="sensor_name" name="sensor_name"><br>
+        <input type="submit" value="Submit">
+    </form>
+    """
+
+
+@app.route("/data")
+def data():
     data = _get_temp_data()
     return f"""
-    <h1>Temperature Sensor</h1>
+    <h1>Temperature Sensor: {data['sensor_name']}</h1>
     <p>Temperature: {data['temperature_celsius']}</p>
     <p>Humidity: {data['humidity']}</p>
+    """
+
+
+@app.route("/")
+def home():
+    return f"""
+    <h1>Temperature Sensor: {_get_sensor_name()}</h1>
+    <p><a href="/data">View Temperature/Humidity Data</a></p>
+    <p><a href="/rename">Rename Sensor</a></p>
     """
 
 
